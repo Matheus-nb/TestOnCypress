6aa4168031a2663ef820bc213d0c9b5ae630b887
Matheus Lascalla <matheusalg.nb@gmail.com>
2022-02-15 11:30:46 -0400

    Teste Aleatório

--- Script Cypress.js
+++ Script Cypress.js
@@ -1,276 +1,148 @@
- // Analytics.js created with Cypress
+// Analytics.js created with Cypress
 //
 // Start writing your Cypress tests below!
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+
+
 context('Actions-page-inicial', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (5 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
+  }
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('#btn-6').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('#btn-7').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('#btn-8').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('#btn-9').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('#btn-10').click()
-  })
+  for (let index = 0; index < 15; index++) {
+    let numberID = Math.floor(Math.random() * (11 - 6)) + 6;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`#btn-${numberID}`).click()
+    })
+  }
 })
+
 
 context('Actions-page-ideias', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/ideias.html')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-6').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-7').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-8').click()
-  })
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (11 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
+  }
 })
 
 
 context('Actions-page-ideiaDaSemana', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/IdeiaDaSemana.html')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-6').click()
-  })
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (11 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
+  }
 })
 
 context('Actions-page-addIdeia', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-6').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-7').click()
-  })
-
-  it('.type() - type into a DOM element', () => {
-    // https://on.cypress.io/type
-    cy.get('.title')
-      .type('fake')
-
-    cy.get('.category')
-      .type('fake')
-
-    cy.get('.image')
-      .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
-
-    cy.get('.description')
-      .type('fake')
-
-    cy.get('.url')
-      .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (11 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
+  }
 
 
-    cy.get('.btn-7').click()
-  })
+  for (let index = 0; index < 5; index++) {
+    it('.type() - type into a DOM element', () => {
+      // https://on.cypress.io/type
+      cy.get('.title')
+        .type('fake')
+
+      cy.get('.category')
+        .type('fake')
+
+      cy.get('.image')
+        .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
+
+      cy.get('.description')
+        .type('fake')
+
+      cy.get('.url')
+        .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
+
+
+      cy.get('.btn-7').click()
+    })
+  }
+
 })
 
 context('Actions-page-contato', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/contato.html')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (11 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
+  }
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
+  for (let index = 0; index < 5; index++) {
+    it('.type() - type into a DOM element', () => {
+      cy.get('.title')
+        .type('fake')
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
+      cy.get('.category')
+        .type('fake')
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
+      cy.get('.description')
+        .type('fake')
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
+      cy.get('.url')
+        .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-6').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-7').click()
-  })
-
-  it('.type() - type into a DOM element', () => {
-    cy.get('.title')
-      .type('fake')
-
-    cy.get('.category')
-      .type('fake')
-
-    cy.get('.description')
-      .type('fake')
-
-    cy.get('.url')
-      .type('https://matheus-nb.github.io/GitPageForAnalytics/ideiasNew.html')
-
-    cy.get('.btn-7').click()
-  })
+      cy.get('.btn-7').click()
+    })
+  }
 })
 
 context('Actions-page-avaliacao', () => {
   beforeEach(() => {
+    cy.wait(1000)
     cy.visit('https://matheus-nb.github.io/GitPageForAnalytics/Avaliacao.html')
   })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-1').click()
-  })
+  for (let index = 0; index < 30; index++) {
+    let numberClass = Math.floor(Math.random() * (11 - 1)) + 1;
+    it('.click() - click into a DOM element', () => {
+      cy.get(`.btn-${numberClass}`).click()
+    })
 
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-2').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-3').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-4').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-5').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-6').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-7').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-8').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-9').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.btn-10').click()
-  })
-
-  it('.click() - click into a DOM element', () => {
-    cy.get('.ratingButton').click()
-  })
+    it('.click() - click into a DOM element', () => {
+      cy.get('.ratingButton').click()
+    })
+  }
 })
